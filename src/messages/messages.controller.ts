ca86a8de0581f96bbca41e53e4779f5a6c56c5d0
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-05-03 13:33:20 +0700

    Error Handling

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -15,8 +22,12 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  show(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async show(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+    return message;
   }
 
   @Post()
