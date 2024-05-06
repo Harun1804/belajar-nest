dec5ee120c747e8db32bb9ed70bd12aa80b4d2f6
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-05-06 11:32:18 +0700

    Add config DB

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
+import { Reports } from './reports/reports.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Reports],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
