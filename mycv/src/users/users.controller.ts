afc661fb73f49a99739e34294b4d4f1260654f1b
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-05-07 09:09:51 +0700

    Add Insert Function

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { createUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  store(@Body() body: createUserDto) {
+    return this.usersService.create(body.email, body.password);
+  }
+}
